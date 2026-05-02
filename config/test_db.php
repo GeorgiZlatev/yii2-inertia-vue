7e922a15585b2717c7cff29897817f63f31e01f9
Georgi Zlatev <gthebox@gmail.com>
2026-05-02 10:51:49 +0300

    CI: run tests on MySQL and add MySQL docker service

--- config/test_db.php
+++ config/test_db.php
@@ -4,6 +4,23 @@ declare(strict_types=1);
 
 use yii\db\Connection;
 
+$driver = getenv('TEST_DB_CONNECTION') ?: 'sqlite';
+
+if ($driver === 'mysql') {
+    $host = getenv('TEST_DB_HOST') ?: '127.0.0.1';
+    $port = getenv('TEST_DB_PORT') ?: '3306';
+    $database = getenv('TEST_DB_DATABASE') ?: 'app_test';
+    $charset = getenv('TEST_DB_CHARSET') ?: 'utf8mb4';
+
+    return [
+        'class' => Connection::class,
+        'dsn' => sprintf('mysql:host=%s;port=%s;dbname=%s', $host, $port, $database),
+        'username' => getenv('TEST_DB_USERNAME') ?: 'root',
+        'password' => getenv('TEST_DB_PASSWORD') ?: '',
+        'charset' => $charset,
+    ];
+}
+
 return [
     'class' => Connection::class,
     'dsn' => 'sqlite:' . dirname(__DIR__) . '/tests/support/data/test.sqlite',
